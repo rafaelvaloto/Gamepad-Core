618093707ed2185c17ae2ad1997e1ad867ade6c4
rafaelvaloto <rafaelvaloto@gmail.com>
2026-01-06 01:41:01 -0300

    Fix indentation in audio-haptics test for consistent formatting

--- Tests/Integration/Features/audio-haptics.test.cpp
+++ Tests/Integration/Features/audio-haptics.test.cpp
@@ -405,7 +405,7 @@ int main(int argc, char* argv[])
 		{
 			Gamepad->DualSenseSettings(0x10, 0x01, 0x01, 0x00, 0x7C, 0xFC, 0x00, 0x00);
 
-		    bControllerFound = true;
+			bControllerFound = true;
 			std::cout << ">>> CONTROLLER CONNECTED! <<<" << std::endl;
 
 			bool bIsWireless = Gamepad->GetConnectionType() == EDSDeviceConnection::Bluetooth;
@@ -414,7 +414,7 @@ int main(int argc, char* argv[])
 			// Set visual feedback
 			Gamepad->SetLightbar({0, 255, 128});
 			Gamepad->SetPlayerLed(EDSPlayer::One, 255);
-		    std::this_thread::sleep_for(std::chrono::milliseconds(50));
+			std::this_thread::sleep_for(std::chrono::milliseconds(50));
 
 			// Get Audio Haptics interface
 			IGamepadAudioHaptics* AudioHaptics = Gamepad->GetIGamepadHaptics();
